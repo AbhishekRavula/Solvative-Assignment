569a79e3f542ba8c0aba714f6ef9c67eaa8edb40
Abhishek Ravula <ravulaabhishek@gmail.com>
2023-09-09 14:10:34 +0530

    removed env port

--- server/src/server.js
+++ server/src/server.js
@@ -1,6 +1,6 @@
 import app from "./app.js";
 
-const port = process.env.PORT || 5005;
+const port = 5005;
 
 app.listen(port, () =>
   console.log(`Server running on port ${port}, http://localhost:${port}`)
